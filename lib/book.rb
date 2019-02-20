11e07af0e4535bf0bc1690a70001b3af847621fc
github username <github email address>
2019-02-20 18:21:53 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,47 @@
+# We'll want the Book class to have properties like a TITLE, an AUTHOR, a PAGE COUNT, etc.
 
+# We'll also want to keep track of all the genres of the Books we create.
 
+# Finally, we'll give our books the total non-sensical ability to turn their own pages.
+
+# learn spec/01_book_spec.rb
+
+class Book
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
